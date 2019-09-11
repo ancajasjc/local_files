93dbf9d9fa97ae7c1af5113d98958d58d537841c
ancajasjc <55198856+ancajasjc@users.noreply.github.com>
2019-09-12 04:26:13 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function findcategory() {
+	var findcat = document.getElementById('findCategory');
+	findcat.classList.add('d-flex','justify-content-center','align-items-center');
+}
+function closemodal() {
+	var closemodal = document.getElementById('findCategory');
+	closemodal.classList.remove('d-flex','justify-content-center','align-items-center');
+}
